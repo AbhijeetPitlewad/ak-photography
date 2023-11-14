69e0f564415c8a857a78d7bbbc15833a65c83d48
AbhijeetPitlewad <19201252000abhijeetvp@gmail.com>
2023-11-14 20:52:32 +0530

    almost project completed, all main things are added0

--- controllers/controllers.js
+++ controllers/controllers.js
@@ -19,10 +19,71 @@ function weddingController(req, res) {
         });
 
     });
+}
 
+
+function preWeddingController(req, res) {
+
+    fs.readdir('./public/prewedding/', function (err, files) {
+        if (err) {
+            return console.log('Unable to scan directory: ' + err);
+        }
+
+        // files.forEach(function (file) {
+        //     console.log(file);
+        // });
+
+        res.render("pre-wedding-photos", {
+            path: "/prewedding/",
+            files
+        });
+
+    });
+}
+
+
+function babyShowerController(req, res) {
+
+    fs.readdir('./public/baby-shower/', function (err, files) {
+        if (err) {
+            return console.log('Unable to scan directory: ' + err);
+        }
+
+        // files.forEach(function (file) {
+        //     console.log(file);
+        // });
+
+        res.render("baby-shower", {
+            path: "/baby-shower/",
+            files
+        });
+
+    });
+}
+
+function albumController(req, res) {
+
+    fs.readdir('./public/album/', function (err, files) {
+        if (err) {
+            return console.log('Unable to scan directory: ' + err);
+        }
+
+        // files.forEach(function (file) {
+        //     console.log(file);
+        // });
+
+        res.render("album", {
+            path: "/album/",
+            files
+        });
+
+    });
 }
 
 module.exports = {
-    weddingController
+    weddingController,
+    preWeddingController,
+    babyShowerController,
+    albumController
 }
 
--- index.js
+++ index.js
@@ -2,39 +2,48 @@ const express = require('express');
 const app = express();
 const path = require('path');
 
-const { weddingController}= require('./controllers/controllers');
+const { weddingController, preWeddingController, babyShowerController, albumController } = require('./controllers/controllers');
 const PORT = 9090;
- 
+
 // Static Middleware
 app.use(express.static(path.join(__dirname, 'public')));
 
 // Set EJS as templating engine 
 app.set('view engine', 'ejs');
 
-app.get("/", (req, res)=>{
+app.get("/", (req, res) => {
     res.render("home");
 })
-app.get("/home", (req, res)=>{
+app.get("/home", (req, res) => {
     res.render("home");
 })
 
-app.get("/about", (req, res)=>{
+app.get("/about", (req, res) => {
     res.render("about");
 })
 
-app.get("/contact", (req, res)=>{
+app.get("/contact", (req, res) => {
     res.render("contact");
 })
 
 
-app.get("/wedding-photos", weddingController );
+app.get("/wedding-photos", weddingController);
+app.get("/pre-wedding-photos", preWeddingController);
+app.get("/baby-shower-photos", babyShowerController);
+app.get("/album", albumController);
+// app.get("/makeup-shoot", weddingController );
+// app.get("/model-photos", weddingController );
 
-app.listen(PORT, (err)=>{
-    if(err){
+app.get("/*", (req, res)=>{
+    res.redirect("/home")
+});
+
+app.listen(PORT, (err) => {
+    if (err) {
         console.log(err);
     }
-    else{
+    else {
         console.log("Server running on PORT: 9090");
     }
-    
+
 })
